c21f6eef420f2b455f251c2f648b9747086786ab
JazzXia <1104841692@qq.com>
2019-11-01 20:59:26 +0800

    修改注册信息中注册的info，以及访问的ip以及名称

--- microservicecloud-provider-dept-8001/src/main/java/com/qtatelier/springcloud/DeptProvider8001_App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/qtatelier/springcloud/DeptProvider8001_App.java
@@ -11,6 +11,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @since 2019
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class DeptProvider8001_App {
     public static void main(String[] args) {
         SpringApplication.run(DeptProvider8001_App.class, args);
