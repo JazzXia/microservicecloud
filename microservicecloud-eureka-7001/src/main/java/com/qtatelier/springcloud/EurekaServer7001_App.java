cfc0bf64f68ffaf17ba019982cb19c6a58f8a11e
JazzXia <1104841692@qq.com>
2019-11-01 20:15:41 +0800

    添加注册中心

--- microservicecloud-eureka-7001/src/main/java/com/qtatelier/springcloud/EurekaServer7001_App.java
+++ microservicecloud-eureka-7001/src/main/java/com/qtatelier/springcloud/EurekaServer7001_App.java
@@ -2,17 +2,19 @@ package com.qtatelier.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @author JazzXia
- * @create 2019-11-01-15:07
+ * @create 2019-11-01-19:19
  * @email jazzxiaw@qq.com
  * @since 2019
  */
 @SpringBootApplication
-public class DeptProvider8001_App {
+@EnableEurekaServer//@EnableEurekaServer服务端启动类，接受其他微服务注册进来
+public class EurekaServer7001_App {
+
     public static void main(String[] args) {
-        SpringApplication.run(DeptProvider8001_App.class, args);
+        SpringApplication.run(EurekaServer7001_App.class,args);
     }
 }
