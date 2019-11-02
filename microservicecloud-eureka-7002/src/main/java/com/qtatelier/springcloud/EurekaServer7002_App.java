907ec25b1def088d2ce18b1aa1f27fe82f6c2ae0
JazzXia <1104841692@qq.com>
2019-11-02 18:10:50 +0800

    集群配置成功。【类似于redis集群】

---
+++ microservicecloud-eureka-7002/src/main/java/com/qtatelier/springcloud/EurekaServer7002_App.java
@@ -0,0 +1,20 @@
+package com.qtatelier.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @author JazzXia
+ * @create 2019-11-01-19:19
+ * @email jazzxiaw@qq.com
+ * @since 2019
+ */
+@SpringBootApplication
+@EnableEurekaServer//@EnableEurekaServer服务端启动类，接受其他微服务注册进来
+public class EurekaServer7002_App {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer7002_App.class,args);
+    }
+}
